f7fcd8952da02afdba9db78bdd042c20add2b1f4
Corentin Crébier <crebier.corentin@gmail.com>
2020-10-01 11:19:59 +0200

    Add transitions on navbar

--- js/navbar.js
+++ js/navbar.js
@@ -1,6 +1,28 @@
-const navButton = document.getElementById('nav-button');
-const navList = document.getElementById('nav-list');
+const navButton = document.getElementById("nav-button");
+const navList = document.getElementById("nav-list");
 
-navButton.addEventListener('click', () => {
-    navList.classList.toggle('active');
+function unrollMenu() {
+  navList.style.height = "unset";
+  const { height } = navList.getBoundingClientRect();
+  navList.style.height = `0`;
+
+  //2 requestAnimationFrame to trigger transition properly
+  window.requestAnimationFrame(() => {
+    window.requestAnimationFrame(() => {
+      navList.style.height = `${height}px`;
+    });
+  });
+}
+
+function rollMenu() {
+  navList.style.height = `0`;
+}
+
+let menuActive = false;
+
+navButton.addEventListener("click", () => {
+  if (menuActive) rollMenu();
+  else unrollMenu();
+
+  menuActive = !menuActive;
 });
