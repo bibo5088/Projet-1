3580d9f209b8dafc9e4693491392c54883cdd1b5
e-floran <e-floran@hotmail.fr>
2020-10-08 14:51:08 +0200

    finished recipes ingredients

--- js/recipe.js
+++ js/recipe.js
@@ -3,6 +3,9 @@ const quantities = document.getElementsByClassName("quantities");
 const quantitiesArray = [...quantities];
 recipePeople.addEventListener("change", function(){
     quantitiesArray.forEach(quantity => {
-        quantity.innerHTML = ((parseInt(quantity.dataset.quantity) * recipePeople.value)/parseInt(recipePeople.dataset.base));
+        if (quantity.classList.contains("eggs")){
+            quantity.innerText = Math.ceil((parseInt(quantity.dataset.quantity) * recipePeople.value)/parseInt(recipePeople.dataset.base));
+        } else{
+            quantity.innerText = (parseInt(quantity.dataset.quantity) * recipePeople.value)/parseInt(recipePeople.dataset.base);}
     });
 })
