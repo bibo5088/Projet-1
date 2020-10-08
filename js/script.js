313b5c6e81643217784d255d5f32de33d9cd4031
Justine Pain <painjustinepro@gmail.com>
2020-10-08 13:13:44 +0200

    changed some classes

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-const message = "Merci pour votre message."
+const message = "Merci pour votre message ! Nous vous répondrons dans les meilleurs délais."
 document.getElementById("contactForm")
         .addEventListener("submit",function(event){
             event.preventDefault();
