49eb954d1dcf2d3c033ed5398b1eaca8f0a5e8a7
Corentin Crébier <crebier.corentin@gmail.com>
2020-10-09 09:18:15 +0200

    Fix broken paths

---
+++ js/form.js
@@ -0,0 +1,9 @@
+const message =
+  "Merci pour votre message.Nous nous efforçons d'y répondre dans les plus bref délais.";
+
+document
+  .getElementById("contactForm")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
+    alert(message);
+  });
--- js/script.js
+++
@@ -1,9 +0,0 @@
-
-const message = "Merci pour votre message.Nous nous efforçons d'y répondre dans les plus bref délais."
-
-document.getElementById("contactForm")
-        .addEventListener("submit",function(event){
-            event.preventDefault();
-            alert(message);
-        });
-        
